5f15bb84d2ca7d016f33d902b34bfa07ed82d924
Vinny Taverna <vinnymac@gmail.com>
2013-11-26 17:45:43 -0500

    Chapter 7 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,6 +24,80 @@ describe "User Pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change( User, :count )
       end
+
+      describe "after submission" do
+
+        describe "password is too short" do
+          before do
+            fill_in "Name",         with: "name"
+            fill_in "Email",        with: "user@example.com"
+            fill_in "Password",     with: "foo"
+            fill_in "Confirmation", with: "foo"
+            click_button submit
+          end
+          it { should have_content('is too short') }
+
+          it { should have_title('Sign up') }
+          it { should have_content('error') }
+        end
+
+        describe "email is invalid" do
+          before do
+            fill_in "Name",         with: "FooBar"
+            fill_in "Email",        with: "example.com"
+            fill_in "Password",     with: "FooBar"
+            fill_in "Confirmation", with: "FooBar"
+            click_button submit
+          end
+          it { should have_content('Email is invalid') }
+
+          it { should have_title('Sign up') }
+          it { should have_content('error') }
+        end
+
+        describe "password can't be blank" do
+          before do
+            fill_in "Name",         with: "FooBar"
+            fill_in "Email",        with: "email@example.com"
+            fill_in "Password",     with: ""
+            fill_in "Confirmation", with: ""
+            click_button submit
+          end
+          it { should have_content("Password can't be blank") }
+
+          it { should have_title('Sign up') }
+          it { should have_content('error') }
+        end
+
+        describe "name is too long" do
+          before do
+            fill_in "Name",         with: "foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoo"
+            fill_in "Email",        with: "email@example.com"
+            fill_in "Password",     with: "FooBar"
+            fill_in "Confirmation", with: "FooBar"
+            click_button submit
+          end
+          it { should have_content('Name is too long (maximum is 50 characters)') }
+
+          it { should have_title('Sign up') }
+          it { should have_content('error') }
+        end
+
+        describe "password confirmation doesn't match" do
+          before do
+            fill_in "Name",         with: "foobar"
+            fill_in "Email",        with: "email@example.com"
+            fill_in "Password",     with: "FooBar"
+            fill_in "Confirmation", with: "BarFoo"
+            click_button submit
+          end
+          it { should have_content("Password confirmation doesn't match Password") }
+
+          it { should have_title('Sign up') }
+          it { should have_content('error') }
+        end
+
+      end
     end
 
     describe "with valid information" do
@@ -36,6 +110,14 @@ describe "User Pages" do
       
       it "should create a user" do
         expect { click_button submit }.to change( User, :count ).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
