47cb93600ddf505e37ccac04a9204f687eee2e7b
Vinny Taverna <vinnymac@gmail.com>
2013-11-26 16:26:48 -0500

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User Pages" do
+
 	subject { page }
 
   describe "profile page" do
@@ -16,5 +17,26 @@ describe "UserPages" do
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change( User, :count )
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      
+      it "should create a user" do
+        expect { click_button submit }.to change( User, :count ).by(1)
+      end
+    end
   end
 end 
