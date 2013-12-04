ffcc285ede560bfb094404d0d12352e6b7b95ecc
Vinny Taverna <vinnymac@gmail.com>
2013-12-03 19:01:43 -0500

    10 through 10.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -66,15 +67,6 @@ class UsersController < ApplicationController
                                     :email, 
                                     :password, 
                                     :password_confirmation )
-    end
-
-    # Before filters
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
     end
 
     def correct_user
